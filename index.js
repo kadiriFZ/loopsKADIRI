ac5aa3636c0cf978611ba579d7abea1fed9d2381
kadiriFZ <kdr.fatiz@gmail.com>
2024-11-21 16:41:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+for(i=0;i<=30;i++){
+    if(i%3==!0){
+        console.log(i)
+    }
+}
+
+
+let fruits=["banana","fraise","poivre"]
+fruits.forEach(function(fruit){
+    console.log(fruit.toUpperCase())
+}
+)
